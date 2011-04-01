f5861cfb8e6587ea7d6dee53f4d4dd4aa6c9fd56
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-04-01 06:30:02 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitForm/trunk@2 512e69e5-c9ed-6342-ab9a-91c975571e94

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * kitForm
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+$module_directory     = 'kit_form';
+$module_name          = 'kitForm';
+$module_function      = 'tool';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8'; 
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License';
+$module_description   = 'Formular generator for KeepInTouch (KIT)'; 
+$module_home          = 'http://phpmanufaktur.de/kit_form';
+$module_guid          = 'F66B6CFC-FA47-4F67-81F2-D4159B65A9E1';
+
+?>
