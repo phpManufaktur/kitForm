172f2934259f61c9e3999abcbf1801c606a0dc0b
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-04-01 08:12:23 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitForm/trunk@3 512e69e5-c9ed-6342-ab9a-91c975571e94

---
+++ languages/DE.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * $project_name$
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ */
+
