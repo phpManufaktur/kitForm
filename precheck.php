b1f05eb8ee70da81728571ef55651deae150b879
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-07-21 05:54:22 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitForm/trunk@29 512e69e5-c9ed-6342-ab9a-91c975571e94

--- precheck.php
+++ precheck.php
@@ -36,7 +36,7 @@ $PRECHECK['WB_VERSION'] = array('VERSION' => '2.8', 'OPERATOR' => '>=');
 $PRECHECK['PHP_VERSION'] = array('VERSION' => '5.2.0', 'OPERATOR' => '>=');
 $PRECHECK['WB_ADDONS'] = array(
 	'dbconnect_le'	=> array('VERSION' => '0.65', 'OPERATOR' => '>='),
-	'dwoo' => array('VERSION' => '0.10', 'OPERATOR' => '>='),
+	'dwoo' => array('VERSION' => '0.11', 'OPERATOR' => '>='),
 	'droplets' => array('VERSION' => '1.0', 'OPERATOR' => '>='),
 	'droplets_extension' => array('VERSION' => '0.16', 'OPERATOR' => '>='),
 	'kit' => array('VERSION' => '0.41', 'OPERATOR' => '>='),
