dad79bf43d5f506fb21f09ebee8a6fe3e701f0ac
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-07-21 06:09:49 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitForm/trunk@31 512e69e5-c9ed-6342-ab9a-91c975571e94

--- precheck.php
+++ precheck.php
@@ -40,7 +40,7 @@ $PRECHECK['WB_ADDONS'] = array(
 	'droplets' => array('VERSION' => '1.0', 'OPERATOR' => '>='),
 	'droplets_extension' => array('VERSION' => '0.16', 'OPERATOR' => '>='),
 	'kit' => array('VERSION' => '0.41', 'OPERATOR' => '>='),
-	'kit_tools' => array('VERSION' => '0.11', 'OPERATOR' => '>=')
+	'kit_tools' => array('VERSION' => '0.14', 'OPERATOR' => '>=')
 );
 
 global $database;  
