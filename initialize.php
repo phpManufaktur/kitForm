9a8b72a6b91c5e1611a292f50e040a3e104cee78
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-07-13 21:26:09 +0200

    Changed initialisation of the Dwoo template engine

--- initialize.php
+++ initialize.php
@@ -64,20 +64,21 @@ if (! class_exists('kitToolsLibrary')) require_once (WB_PATH . '/modules/kit_too
 
 require_once (WB_PATH . '/modules/' . basename(dirname(__FILE__)) . '/class.form.php');
 
-// initialize Dwoo
-global $parser;
+if (!class_exists('Dwoo'))
+  require_once WB_PATH.'/modules/dwoo/include.php';
 
-if (!class_exists('Dwoo')) {
-    require_once WB_PATH.'/modules/dwoo/include.php';
+// initialize the template engine
+global $parser;
+if (!is_object($parser)) {
+  $cache_path = WB_PATH.'/temp/cache';
+  if (!file_exists($cache_path)) mkdir($cache_path, 0755, true);
+  $compiled_path = WB_PATH.'/temp/compiled';
+  if (!file_exists($compiled_path)) mkdir($compiled_path, 0755, true);
+  $parser = new Dwoo($compiled_path, $cache_path);
 }
-
-$cache_path = WB_PATH.'/temp/cache';
-if (!file_exists($cache_path)) mkdir($cache_path, 0755, true);
-$compiled_path = WB_PATH.'/temp/compiled';
-if (!file_exists($compiled_path)) mkdir($compiled_path, 0755, true);
-
-global $parser;
-if (!is_object($parser)) $parser = new Dwoo($compiled_path, $cache_path);
+// load extensions for the template engine
+$loader = $parser->getLoader();
+$loader->addDirectory(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/htt/plugins/');
 
 global $kitLibrary;
 if (! is_object($kitLibrary)) $kitLibrary = new kitToolsLibrary();
