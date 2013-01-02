166a9308e427edf61d363a63fe129c289db4ceea
Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
2013-01-03 00:59:34 +0100

    updated info.php

--- info.php
+++ info.php
@@ -5,7 +5,7 @@
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://phpmanufaktur.de
- * @copyright 2011 - 2012
+ * @copyright 2011 - 2013
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
@@ -34,7 +34,7 @@ else {
 $module_directory = 'kit_form';
 $module_name = 'kitForm';
 $module_function = 'tool';
-$module_version = '0.40';
+$module_version = '0.41';
 $module_status = 'Stable';
 $module_platform = '2.8';
 $module_author = 'Ralf Hertsch, Berlin (Germany)';
